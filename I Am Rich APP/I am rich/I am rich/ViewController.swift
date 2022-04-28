1ac6cf7ddbe1d22cb056782a5a7964c631e27d72
juampebarrios <78377376+juampebarrios@users.noreply.github.com>
2022-04-27 22:27:29 -0300

    Add files via upload

---
+++ I Am Rich APP/I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Juan Pedro Barrios on 9/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
